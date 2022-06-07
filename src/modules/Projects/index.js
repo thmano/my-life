e21b8a9f704e42b4dde0face8e8b0d5ec36d03a9
Thiago Cavalcanti <thiago.cavalcanti@hiplatform.com.br>
2022-06-07 20:27:57 -0300

    conectando com api do gitHub

--- src/modules/Projects/index.js
+++ src/modules/Projects/index.js
@@ -1,12 +1,33 @@
+import { useState, useEffect } from 'react'
 import Menu from "../Menu"
+
 import './style.css'
 
 const Projects = () => {
+
+  const [repos, setRepos ] = useState([]) 
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/thmano/repos')
+    .then( response => response.json())
+    .then( data => setRepos(data) )
+  },[])
     return(
         <div>
              <Menu />
              <div className="display">
             <h1>Projetos</h1>
+            <ul>
+            {repos.map(data  => {
+              return(
+                <>
+                  <li>Nome: {data.name}</li>
+                  <li>Descirption: {data.description}</li>
+                  <br/>
+                </>
+              )
+            })}
+            </ul>
 
           </div>
         </div>
