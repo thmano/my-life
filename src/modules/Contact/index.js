5799c4fdcbc4f2a9e75e52b23242a3f40947c95d
Thiago Cavalcanti <thiago.cavalcanti@hiplatform.com.br>
2022-06-08 10:12:34 -0300

    iniciando criação de tema dark

--- src/modules/Contact/index.js
+++ src/modules/Contact/index.js
@@ -1,12 +1,25 @@
 import Menu from "../Menu"
 import './style.css'
+import Button from '@mui/material/Button';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Contact = () => {
  return(
      <div>
           <Menu />
           <div className="display">
-            <h1>Contato</h1>
+            <div className="contact_buttons">
+              <Button variant="contained" color="success" startIcon={<WhatsAppIcon />}>
+                WhatsApp
+              </Button>
+              <Button variant="contained" color="primary" startIcon={<LinkedInIcon />}>
+                Linkedin
+              </Button>
+              <Button variant="contained" color="warning">
+                thiago_cavalcanti@live.com
+              </Button>
+            </div>
 
           </div>
      </div>
--- src/modules/Menu/index.js
+++ src/modules/Menu/index.js
@@ -1,11 +1,34 @@
+import { useState, useEffect } from 'react'
+
 import Image from '../../components/Image';
 import imageLogo from '../../assets/logo.png'
-import { MdOutlineLogout } from 'react-icons/md'
+import NightlightIcon from '@mui/icons-material/Nightlight';
+import GTranslateIcon from '@mui/icons-material/GTranslate';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
 import './style.css';
 import List from '../../router/List';
 
 
 const Menu = () =>{
+
+    const [theme, setTheme] = useState(false)
+
+    useEffect(() => {
+
+        if (theme === true) {
+            document.querySelector('.header').style.backgroundColor = '#2b2828'
+            document.querySelector('.side_menu').style.backgroundColor = '#2b2828'
+            document.querySelector('.header_menu').style.color = '#fff'
+            document.querySelector('.link').style.color = '#fff'
+        }else{
+            document.querySelector('.header').style.backgroundColor = '#fff'
+            document.querySelector('.header_menu').style.color = '#5f5f5f'
+            document.querySelector('.side_menu').style.backgroundColor = '#fff'
+            document.querySelector('.link').style.color = '#282c34'
+            
+        }
+
+    },[theme])
 
 return (
 <div className='container'>
@@ -18,7 +41,13 @@ return (
             height={'60px'} />
         </div>
         <div className='header_menu'>
-            < MdOutlineLogout className='logout_icon' />
+            {theme === false && 
+                < NightlightIcon onClick={() => setTheme( true ) } className='logout_icon' />
+            }
+            {theme === true && 
+                < Brightness4Icon onClick={() => setTheme( false ) } className='logout_icon' />
+            }
+            < GTranslateIcon className='logout_icon' />
         </div>
     </div>
     <div className='side_menu'>
